8d14538f31371ccd92bd90b25fc5b7bbe5ad6f5d
Mehran <mehran.rasulian@gmail.com>
2024-02-01 20:13:41 +0330

    Updated the space between sections

--- source/index.blade.php
+++ source/index.blade.php
@@ -52,7 +52,7 @@
         </div>
     </section>
 
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="px-4 pt-12 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-20 lg:pt-16">
         <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <div>
                 <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
