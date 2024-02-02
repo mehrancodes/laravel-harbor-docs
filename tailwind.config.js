22c50cd11c27746ded079338db9d9d73cc2c0e94
Mehran <mehran.rasulian@gmail.com>
2024-02-02 17:16:08 +0330

    added the inter font for documentation layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [
-          'Montserrat'
+          'Montserrat', 'inter'
         ],
         mono: [
           'monospace',
